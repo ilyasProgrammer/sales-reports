d1c49cd55f0bb8c806ee3beb83a1911b97023805
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-10-15 03:45:50 +0500

    report profit loss

---
+++ profitloss/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'profit loss',
+    'version' : '1.1',
+    'author' : 'Ilyas',
+    'category' : 'Accounting & Finance',
+    'description' : """ """,
+    'website': '',
+    'depends' : ['base_setup', 'product', 'analytic', 'board', 'edi', 'report'],
+    'data': [
+        'report/profit_loss_report_view.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ profitloss/report/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+import profit_loss_report
